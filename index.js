ef15a899ecbe742297cf37422b22d6dfeefe9a08
Casey Halbohn <wrxbeauty15@hotmail.com>
2023-06-11 11:56:37 -0400

    Added GET route

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ const express = require('express')
 // Initialize app
 const app = express()
 
-
+// Create Route
+app.get('/', function (req, res) {
+    // This gets sent to client
+    res.send('Hey there!')
+})
