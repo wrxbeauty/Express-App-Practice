2fc47170dd9a10c5e25b23f344d34f2e3c77860a
Casey Halbohn <wrxbeauty15@hotmail.com>
2023-06-12 12:02:50 -0400

    Finished express practice

--- index.js
+++ index.js
@@ -21,36 +21,25 @@ app.get('/', function (req, res) {
 
 // Create Second Route
 app.get('/second', function (req, res) {
-    res.send('<h1 style="text-align:center">You Made It!</h1>')
+    res.send(`
+        <body>
+            <h1 style="text-align:center">You Made It!</h1>
+        </body>
+    `)
 })
 
 // Teal Page
-app.get('/teal', function (req, res) {
+app.get('/:color', function (req, res) {
+    let myColor = req.params.color
     res.send(`
         <body style="margin: 0;">
             <div style="border: 1px solid black; height: 10vh; background-color: teal;">
                 <h2 style="text-align: center;">NAV BAR</h2>
             </div>
-            <h1 style="color: teal;">Teal Page</h1>
+            <h1 style="color: ${myColor};">${myColor} Page</h1>
         </body>
     `)
 })
 
-// Crimson Page
-// app.get('/teal', function (req, res) {
-//     res.send(`
-//         <body style="margin: 0;">
-//             <div style="border: 1px solid black; height: 10vh; background-color: crimson;">
-//                 <h2 style="text-align: center;">NAV BAR</h2>
-//             </div>
-//             <h1 style="color: crimson;">Crimson Page</h1>
-//         </body>
-//     `)
-// })
-
-
 // Listen 
-// app.listen(3000, function () {
-//     console.log('I am working!')
-// })
 app.listen(process.env.PORT)
