e24dc34b50582ac418637d634b00699ca21bb1fb
Casey Halbohn <wrxbeauty15@hotmail.com>
2023-06-12 09:38:51 -0400

    Added Teal and Crimson routes

--- index.js
+++ index.js
@@ -9,14 +9,45 @@ const app = express()
 
 // Create Route
 app.get('/', function (req, res) {
-    // This gets sent to client
-    res.send('<h1 style="color:purple">Hey there!</h1>')
+    res.send(`
+        <body style="margin: 0;">
+            <div style="border: 1px solid black; height: 10vh; background-color: white;">
+                <h2 style="text-align: center;">NAV BAR</h2>
+            </div>
+            <h1>White Page</h1>
+        </body>
+    `)
 })
 
 // Create Second Route
 app.get('/second', function (req, res) {
     res.send('<h1 style="text-align:center">You Made It!</h1>')
 })
+
+// Teal Page
+app.get('/teal', function (req, res) {
+    res.send(`
+        <body style="margin: 0;">
+            <div style="border: 1px solid black; height: 10vh; background-color: teal;">
+                <h2 style="text-align: center;">NAV BAR</h2>
+            </div>
+            <h1 style="color: teal;">Teal Page</h1>
+        </body>
+    `)
+})
+
+// Crimson Page
+// app.get('/teal', function (req, res) {
+//     res.send(`
+//         <body style="margin: 0;">
+//             <div style="border: 1px solid black; height: 10vh; background-color: crimson;">
+//                 <h2 style="text-align: center;">NAV BAR</h2>
+//             </div>
+//             <h1 style="color: crimson;">Crimson Page</h1>
+//         </body>
+//     `)
+// })
+
 
 // Listen 
 // app.listen(3000, function () {
